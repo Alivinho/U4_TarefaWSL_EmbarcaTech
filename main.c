fbc1baf8008de972a2522f3a1063f3e8850319d0
Alisson Livio <livilia74629@gmail.com>
2025-02-04 17:15:13 -0300

    estrutura inicial do projeto

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ws2812.pio.h"
+
+#define WS2812_PIN 7
+#define IS_RGBW false
+#define NUM_PIXELS 25
+
+
+int main() {
+    stdio_init_all();
+    
+
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+
+    while (1) {}
+
+    return 0;
+}
