42d29f856918d45d86cbc70ca3c9889900562966
Alisson Livio <livilia74629@gmail.com>
2025-02-05 20:35:24 -0300

    feat: exibir string no display oled

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "ws2812.pio.h"
+#include "hardware/i2c.h"
+#include "includes/ssd1306.h"
+#include "includes/font.h"
 
 // Definição de Pinos do LED RGB
 #define LED_RED_PIN 13
@@ -14,10 +17,16 @@
 #define BUTTON_A_PIN 5
 #define BUTTON_B_PIN 6
 
-
+// Matriz de LEDS
 #define WS2812_PIN 7
 #define IS_RGBW false
 #define NUM_PIXELS 25
+
+// Display Oled - SSD1306
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
 
 //extern void animacao_1(PIO pio, uint sm, uint numero_atual);
 
@@ -42,28 +51,43 @@ int main() {
 
     initLedButtons();
     
-
+    // Configuração do PIO e da Matriz de LED
     PIO pio = pio0;
     int sm = 0;
     uint offset = pio_add_program(pio, &ws2812_program);
-
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
+    // Configuração (Inicialuzação) do Display Oled
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); 
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); 
+    gpio_pull_up(I2C_SDA); 
+    gpio_pull_up(I2C_SCL); 
+    ssd1306_t ssd; 
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); 
+    ssd1306_config(&ssd); 
+    ssd1306_send_data(&ssd);
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
 
 
     //animacao_1(pio, sm, numero_atual);
 
-    while (1){
-        if(stdio_usb_connected()){
-            char c;
+    bool cor = true;
+    while (true)
+    {
+        cor = !cor;
+        // Atualiza o conteúdo do display com animações
+        ssd1306_fill(&ssd, !cor); // Limpa o display
+        ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
+        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
+        ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48); // Desenha uma string      
+        ssd1306_send_data(&ssd); // Atualiza o display
 
-            scanf("%c", &c);
-
-            if (c == 'r'){
-                gpio_put(LED_RED_PIN, 1);
-            }
-        }
-
+        sleep_ms(1000);
     }
 
     return 0;
@@ -85,3 +109,4 @@ void initLedButtons(){
     gpio_init(LED_GREEN_PIN);
     gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
 }
+
