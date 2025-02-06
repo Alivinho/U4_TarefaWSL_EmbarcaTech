99c1eafa925d735188a8c88afc1a104cd3abde79
Alisson Livio <livilia74629@gmail.com>
2025-02-05 21:23:16 -0300

    feat: leitura de numero do monitor serial e exibição na matriz de led

--- main.c
+++ main.c
@@ -28,21 +28,19 @@
 #define I2C_SCL 15
 #define endereco 0x3C
 
-//extern void animacao_1(PIO pio, uint sm, uint numero_atual);
+extern void animacao_1(PIO pio, uint sm, uint numero_atual);
 
 // Protótipos das Funções utilizadas
 void initLedButtons();
 
-static volatile uint numero_atual = 0;   // Variável para armazenar o número atual
-
 const uint32_t BRIGHTNESS = 0xCC; // Ajusta o brilho aqui (0x00 para apagado, 0xFF para brilho máximo)
 
 // Acionamento da matriz de LEDs - ws2812b
-/*void desenhar_matriz(PIO pio, uint sm, const uint32_t *desenho) {
+void desenhar_matriz(PIO pio, uint sm, const uint32_t *desenho) {
     for (int i = 0; i < NUM_PIXELS; i++) { // Aplica a cor ao padrão
        pio_sm_put_blocking(pio, sm, (desenho[24-i]) * BRIGHTNESS); 
     }
-}*/
+}
 
 
 int main() {
@@ -73,18 +71,30 @@ int main() {
     ssd1306_send_data(&ssd);
 
 
-    //animacao_1(pio, sm, numero_atual);
-
     bool cor = true;
-    while (true)
-    {
+    while (true){
         cor = !cor;
+
+        if (stdio_usb_connected()){
+            char caractere;
+
+            printf("Digite um caractere ou número: ");
+            scanf(" %c", &caractere);
+
+            if (caractere >= '0' && caractere <= '9') {
+                // Se for um número (0 a 9), exibe na matriz WS2812
+                uint numero_atual = (uint)(caractere - '0');  // Converte diretamente o caractere para inteiro
+
+                animacao_1(pio, sm, numero_atual);
+            } 
+        }
         // Atualiza o conteúdo do display com animações
         ssd1306_fill(&ssd, !cor); // Limpa o display
         ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
-        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
-        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
-        ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48); // Desenha uma string      
+        //ssd1306_draw_char(&ssd,'A', 8, 10);
+        ssd1306_draw_string(&ssd, "CARACTERE", 8, 10); // Desenha uma string
+        //ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
+        //ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48); // Desenha uma string      
         ssd1306_send_data(&ssd); // Atualiza o display
 
         sleep_ms(1000);
