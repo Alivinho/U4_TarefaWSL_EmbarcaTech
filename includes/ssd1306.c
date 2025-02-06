cdd6a04a5702aec48a2773d94b8fcabda1aa75d6
Alisson Livio <livilia74629@gmail.com>
2025-02-06 13:17:22 -0300

    feat: função reconhecimento das letras minúsculas

--- includes/ssd1306.c
+++ includes/ssd1306.c
@@ -157,13 +157,18 @@ void ssd1306_vline(ssd1306_t *ssd, uint8_t x, uint8_t y0, uint8_t y1, bool value
 void ssd1306_draw_char(ssd1306_t *ssd, char c, uint8_t x, uint8_t y)
 {
   uint16_t index = 0;
-  char ver=c;
+  
   if (c >= 'A' && c <= 'Z')
   {
     index = (c - 'A' + 11) * 8; // Para letras maiúsculas
-  }else  if (c >= '0' && c <= '9')
+  }
+  else if (c >= 'a' && c <= 'z') // Adiciona suporte para letras minúsculas
   {
-    index = (c - '0' + 1) * 8; // Adiciona o deslocamento necessário
+    index = (c - 'a' + 11 + 26) * 8; // Para letras minúsculas, após as maiúsculas
+  }
+  else if (c >= '0' && c <= '9')
+  {
+    index = (c - '0' + 1) * 8; // Para números
   }
   
   for (uint8_t i = 0; i < 8; ++i)
@@ -175,6 +180,7 @@ void ssd1306_draw_char(ssd1306_t *ssd, char c, uint8_t x, uint8_t y)
     }
   }
 }
+
 
 // Função para desenhar uma string
 void ssd1306_draw_string(ssd1306_t *ssd, const char *str, uint8_t x, uint8_t y)
