841b3f9ea2c4c22e4f30177876150bb1a0b32459
Alisson Livio <livilia74629@gmail.com>
2025-02-07 17:39:53 -0300

    feat: Exibir estado do led no Display

--- main.c
+++ main.c
@@ -15,7 +15,6 @@
 #define UART_RX_PIN 1    
 
 // Definição de Pinos do LED RGB
-#define LED_RED_PIN 13
 #define LED_BLUE_PIN 12
 #define LED_GREEN_PIN 11
 
@@ -33,26 +32,23 @@
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
+ssd1306_t ssd; 
 
+// Definição dos variáveis para controle da Rotina de Interrupção
 #define DEBOUNCE_DELAY 200  
-
 volatile uint32_t last_interrupt_timeA = 0;
 volatile uint32_t last_interrupt_timeB = 0;
 
+// Função para dese
 extern void animacao_1(PIO pio, uint sm, uint numero_atual);
 
 // Protótipos das Funções utilizadas
 void initLedButtons();
 void button_callback(uint gpio, uint32_t events);
+void desenhar_matriz(PIO pio, uint sm, const uint32_t *desenho);
+void exibir_caractere_no_display(char caractere);
 
 const uint32_t BRIGHTNESS = 0xCC; // Ajusta o brilho aqui (0x00 para apagado, 0xFF para brilho máximo)
-
-// Acionamento da matriz de LEDs - ws2812b
-void desenhar_matriz(PIO pio, uint sm, const uint32_t *desenho) {
-    for (int i = 0; i < NUM_PIXELS; i++) { // Aplica a cor ao padrão
-       pio_sm_put_blocking(pio, sm, (desenho[24-i]) * BRIGHTNESS); 
-    }
-}
 
 
 int main() {
@@ -65,8 +61,6 @@ int main() {
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    //stdio_set_driver_enabled(&stdio_uart, true);
-
     
     // Configuração do PIO e da Matriz de LED
     PIO pio = pio0;
@@ -80,22 +74,15 @@ int main() {
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); 
     gpio_pull_up(I2C_SDA); 
     gpio_pull_up(I2C_SCL); 
-    ssd1306_t ssd; 
     ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); 
     ssd1306_config(&ssd); 
     ssd1306_send_data(&ssd);
 
-    // Limpa o display. O display inicia com todos os pixels apagados.
-    ssd1306_fill(&ssd, false);
-    ssd1306_send_data(&ssd);
-
+    // Chamada de funções para Interrupção dos botões 
     gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback);
-    gpio_set_irq_enabled(BUTTON_B_PIN, GPIO_IRQ_EDGE_FALL, true);
-
+    gpio_set_irq_enabled_with_callback(BUTTON_B_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback);
 
     bool cor = true;
-
-    uart_putc_raw(UART_ID, 'A');
 
     // Limpa o display
     ssd1306_fill(&ssd, false);
@@ -103,9 +90,8 @@ int main() {
 
     // Desenha o retângulo uma única vez
     ssd1306_rect(&ssd, 3, 3, 122, 58, true, false);  // Desenha o retângulo (com cor)
-    ssd1306_send_data(&ssd);  // Atualiza o displa
-
-    
+    ssd1306_draw_string(&ssd, "ESPERANDO...", 25, 30); // Desenha uma string        
+    ssd1306_send_data(&ssd);  // Atualiza o display
 
     while (true){
         cor = !cor;
@@ -113,7 +99,7 @@ int main() {
         if (stdio_usb_connected()){
             char caractere;
 
-            printf("Digite um caractere ou número: ");
+            printf("Digite um caractere ou número:\n");
             scanf(" %c", &caractere);
 
             if (caractere >= '0' && caractere <= '9') {
@@ -122,9 +108,7 @@ int main() {
 
                 animacao_1(pio, sm, numero_atual);
             }else{
-                // Exibe o caractere no display Oled
-                ssd1306_draw_char(&ssd, caractere, 64, 32);
-                ssd1306_send_data(&ssd); // Atualiza o display
+                exibir_caractere_no_display(caractere);
             }
         }
 
@@ -145,9 +129,6 @@ void initLedButtons(){
     gpio_set_dir(BUTTON_B_PIN, GPIO_IN);
     gpio_pull_up(BUTTON_B_PIN);
 
-    gpio_init(LED_RED_PIN);
-    gpio_set_dir(LED_RED_PIN, GPIO_OUT);
-
     gpio_init(LED_BLUE_PIN);
     gpio_set_dir(LED_BLUE_PIN, GPIO_OUT);
     
@@ -155,6 +136,15 @@ void initLedButtons(){
     gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
 }
 
+
+// Acionamento da matriz de LEDs - ws2812b
+void desenhar_matriz(PIO pio, uint sm, const uint32_t *desenho) {
+    for (int i = 0; i < NUM_PIXELS; i++) { // Aplica a cor ao padrão
+       pio_sm_put_blocking(pio, sm, (desenho[24-i]) * BRIGHTNESS); 
+    }
+}
+
+// Funão da rotina de interrupção 
 void button_callback(uint gpio, uint32_t events) {
     uint32_t tempo_atual = to_ms_since_boot(get_absolute_time());
 
@@ -164,15 +154,12 @@ void button_callback(uint gpio, uint32_t events) {
         static bool led_state_a = false;
         led_state_a = !led_state_a;
         gpio_put(LED_GREEN_PIN, led_state_a);
-
-        // Mensagem de status do LED Verde
-        //char mensagem[50];
-        //snprintf(mensagem, sizeof(mensagem), "Botão A pressionado: LED Verde %s\n", led_state_a ? "LIGADO" : "DESLIGADO");
-        printf("\nBotão A pressionado: LED Verde %s\n", led_state_a ? "LIGADO" : "DESLIGADO");
-
-        // Enviar mensagem via UART
-        //uart_puts(UART_ID, mensagem);
-
+        
+        printf("\nBotão A pressionado: LED Verde %s", led_state_a ? "LIGADO" : "DESLIGADO");
+       
+        ssd1306_fill(&ssd, false);  
+        ssd1306_draw_string(&ssd, led_state_a ? "LED VERDE ON" : "LED VERDE OFF", 18, 30);
+        ssd1306_send_data(&ssd);  
     }
     else if (gpio == BUTTON_B_PIN) {
         if (tempo_atual - last_interrupt_timeB < DEBOUNCE_DELAY ) return;
@@ -181,8 +168,21 @@ void button_callback(uint gpio, uint32_t events) {
         led_state_b = !led_state_b;
         gpio_put(LED_BLUE_PIN, led_state_b);
 
-        printf("\nBotão B pressionado: LED Azul %s\n", led_state_b ? "LIGADO" : "DESLIGADO");
+        printf("\nBotão B pressionado: LED Azul %s", led_state_b ? "LIGADO" : "DESLIGADO");
+
+        ssd1306_fill(&ssd, false);  
+        ssd1306_draw_string(&ssd, led_state_b ? "LED AZUL ON" : "LED AZUL OFF", 18, 30);
+        ssd1306_send_data(&ssd); 
 
     }
+}
+
+void exibir_caractere_no_display(char caractere){
+
+    ssd1306_fill(&ssd, false);
+    ssd1306_rect(&ssd, 3, 3, 122, 58, true, false);  // Desenha o retângulo (com cor)
+    ssd1306_draw_char(&ssd, caractere, 62, 32);
+    ssd1306_send_data(&ssd); // Atualiza o display
+
 }
 
